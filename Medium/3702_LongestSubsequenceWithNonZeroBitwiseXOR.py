9416a1771971c02049a5d7c672cf74f157a0b1ef
Jhansi <sarevalasajhansi@gmail.com>
2026-08-16 20:09:03 +0530

    Leetcode 2029 Solution

---
+++ Medium/3702_LongestSubsequenceWithNonZeroBitwiseXOR.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestSubsequence(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        xor = 0
+        has_non_zero = False
+
+        for num in nums:
+            xor ^= num
+            if num != 0:
+                has_non_zero = True
+
+        if xor != 0:
+            return len(nums)
+
+        if has_non_zero:
+            return len(nums) - 1
+
+        return 0
